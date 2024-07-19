324666e433f71f31e70422e5db79144b82bccaf8
AmanSingh494 <amanforwork1@gmail.com>
2024-07-19 20:32:40 +0530

    Added Css and Js for contact page

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,10 +4,8 @@ allInputBoxDiv.forEach((inputBoxDiv) => {
   const inputBoxTextDiv = inputBoxDiv.querySelector('.input-box-text-div')
   inputBox.addEventListener('focus', () => {
     inputBoxTextDiv.style.display = 'none'
-    inputBoxDiv.style.border = '1px solid #007bff'
   })
   inputBoxDiv.addEventListener('focusout', (e) => {
-    inputBoxDiv.style.border = '1px solid #ced4da'
     if (e.target.value === '') {
       inputBoxTextDiv.style.display = 'flex'
     }
