f01dd94e69b5be115ac8597ef605dcce90deb903
adarsh <adarshmaurya9415@gmail.com>
2024-08-07 13:22:14 +0530

    db setup done

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  user: process.env.PSQLUSER,
-  host: process.env.PSQLHOST,
-  database: process.env.PSQLDATABASE,
-  password: process.env.PSQLPASSWORD,
-  port: process.env.PSQLPORT
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 module.exports = pool
